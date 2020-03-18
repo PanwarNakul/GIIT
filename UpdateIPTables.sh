4c5ca0ee978d1fbfc596bd56b79822237c301f45
panwarnakul <nakul.panwar77@gmail.com>
2020-03-18 18:53:47 +0530

    adding Iptables update playbook

---
+++ UpdateIPTables.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+iptables-save > /root/iptables_BKP_$(date +"%d-%b-%Y")
+iptables -A INPUT -s 10.106.0.1/255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 10.106.0.13/255.255.255.255 -j ACCEPT
+iptables -A OUTPUT -s 10.106.0.1/255.255.255.255 -j ACCEPT
+iptables -A OUTPUT -s 10.106.0.13/255.255.255.255 -j ACCEPT
+/etc/init.d/iptables save
+cp /etc/hosts.allow /etc/hosts.allow_BKP_$(date +"%d-%b-%Y")
+sed -i "s/^ALL:.*/&, 10.106.0.1\/255.255.255.255, 10.106.0.13\/255.255.255.255/" /etc/hosts.allow
