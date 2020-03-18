3cb90670c501ba7519c0d661616cdf662f5b31d5
panwarnakul <nakul.panwar77@gmail.com>
2020-03-18 16:13:32 +0530

    kernel version check and set the reboot flag

---
+++ CheckKernelReboot.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+LAST_KERNEL=$(awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg | cut  -c  22-51 |  head -1)
+       CURRENT_KERNEL=$(uname -r)
+       if [[ $LAST_KERNEL == $CURRENT_KERNEL ]]; then
+        echo "Reboot is required" > /tmp/reboot
+        cat /tmp/reboot
+       else
+        echo "Kernel is same and not required." > /tmp/noreboot
+        cat /tmp/noreboot
+       fi
+
