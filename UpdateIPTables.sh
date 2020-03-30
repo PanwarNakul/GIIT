5cdaaf8d274fd4df1333c8330c70956c9642d718
panwarnakul <nakul.panwar77@gmail.com>
2020-03-30 11:22:21 +0530

    add 10.105.0.13 and 10.104.0.13 in Test branch

--- UpdateIPTables.sh
+++ UpdateIPTables.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 iptables-save > /root/iptables_BKP_$(date +"%d-%b-%Y")
-iptables -A INPUT -s 10.106.0.1/255.255.255.255 -j ACCEPT
-iptables -A INPUT -s 10.106.0.13/255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 10.105.0.1/255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 10.104.0.13/255.255.255.255 -j ACCEPT
 iptables -A OUTPUT -s 10.106.0.1/255.255.255.255 -j ACCEPT
 iptables -A OUTPUT -s 10.106.0.13/255.255.255.255 -j ACCEPT
 /etc/init.d/iptables save
