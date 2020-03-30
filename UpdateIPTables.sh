0bbfdc22ed9a0bbe54a04e942a55a31796f9c862
panwarnakul <nakul.panwar77@gmail.com>
2020-03-30 11:42:59 +0530

    CHecking the feature

--- UpdateIPTables.sh
+++ UpdateIPTables.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 iptables-save > /root/iptables_BKP_$(date +"%d-%b-%Y")
-iptables -A INPUT -s 10.105.0.1/255.255.255.255 -j ACCEPT
-iptables -A INPUT -s 10.104.0.13/255.255.255.255 -j ACCEPT
-iptables -A OUTPUT -s 10.106.0.1/255.255.255.255 -j ACCEPT
-iptables -A OUTPUT -s 10.106.0.13/255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 10.109.0.1/255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 10.119.0.13/255.255.255.255 -j ACCEPT
+iptables -A OUTPUT -s 10.102.0.1/255.255.255.255 -j ACCEPT
+iptables -A OUTPUT -s 10.103.0.13/255.255.255.255 -j ACCEPT
 /etc/init.d/iptables save
 cp /etc/hosts.allow /etc/hosts.allow_BKP_$(date +"%d-%b-%Y")
 sed -i "s/^ALL:.*/&, 10.106.0.1\/255.255.255.255, 10.106.0.13\/255.255.255.255/" /etc/hosts.allow
