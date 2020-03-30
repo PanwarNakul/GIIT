d7cb86bfbe90a0eea62b8f843073621643f1fde8
panwarnakul <nakul.panwar77@gmail.com>
2020-03-30 11:44:22 +0530

    changes from Test

--- UpdateIPTables.sh
+++ UpdateIPTables.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 iptables-save > /root/iptables_BKP_$(date +"%d-%b-%Y")
-iptables -A INPUT -s 10.109.0.1/255.255.255.255 -j ACCEPT
-iptables -A INPUT -s 10.119.0.13/255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 10.110.0.1/255.255.255.255 -j ACCEPT
+iptables -A INPUT -s 10.111.0.13/255.255.255.255 -j ACCEPT
 iptables -A OUTPUT -s 10.102.0.1/255.255.255.255 -j ACCEPT
 iptables -A OUTPUT -s 10.103.0.13/255.255.255.255 -j ACCEPT
 /etc/init.d/iptables save
